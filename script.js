6a4b4c4fef639f8e3f090caab606b9b14141183f
Max G <gmax0687@gmail.com>
2020-06-08 21:56:24 -0500

    added keyboard functionality, use switch statements now

--- script.js
+++ script.js
@@ -3,46 +3,8 @@ let numbers = [];
 
 const screen = document.querySelector(".screen");
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", clearScreen);
-const back = document.querySelector("#back");
-back.addEventListener("click", backspace);
-
-//creates buttons for all the numbers and operators
-const zero = document.querySelector("#zero");
-zero.addEventListener("click", () => addTextToScreen(0));
-const one = document.querySelector("#one");
-one.addEventListener("click", () => addTextToScreen(1));
-const two = document.querySelector("#two");
-two.addEventListener("click", () => addTextToScreen(2));
-const three = document.querySelector("#three");
-three.addEventListener("click", () => addTextToScreen(3));
-const four = document.querySelector("#four");
-four.addEventListener("click", () => addTextToScreen(4));
-const five = document.querySelector("#five");
-five.addEventListener("click", () => addTextToScreen(5));
-const six = document.querySelector("#six");
-six.addEventListener("click", () => addTextToScreen(6));
-const seven = document.querySelector("#seven");
-seven.addEventListener("click", () => addTextToScreen(7));
-const eight = document.querySelector("#eight");
-eight.addEventListener("click", () => addTextToScreen(8));
-const nine = document.querySelector("#nine");
-nine.addEventListener("click", () => addTextToScreen(9));
-
-const add = document.querySelector("#add");
-add.addEventListener("click", () => getOperators(" + "));
-const subtract = document.querySelector("#subtract");
-subtract.addEventListener("click", () => getOperators(" - "));
-const multiply = document.querySelector("#multiply");
-multiply.addEventListener("click", () => getOperators(" * "));
-const divide = document.querySelector("#divide");
-divide.addEventListener("click", () => getOperators(" / "));
-const equals = document.querySelector("#equals");
-equals.addEventListener("click", () => operate(screenContent.join("")));
-
-const point = document.querySelector("#decimal");
-point.addEventListener("click", () => addDecimal());
+document.addEventListener("keydown", keyPush);
+document.addEventListener("click", keyPush);
 
 //pushes inputs to the calculators screen
 function addTextToScreen(number) {
@@ -88,10 +50,12 @@ function operate(lastNumber) {
         return new Function("return " + fn)();
     }
 
-    numbers = [];
+    numbers = [unpolishedAnswer];
     screenContent = [];
 }
 
+//checks to see if there is a decimal in the number, if there is dissallows
+//another decimal input
 function addDecimal() {
     const dot = /[.]/;
     let decTest = dot.test(screenContent.join(""));
@@ -103,19 +67,103 @@ function addDecimal() {
     }
 }
 
-function checkAnswerLength(thing) {
-    let arrayOfThing = [...thing];
+function checkAnswerLength(answer) {
+    console.log(answer);
+    let answerArray = [...answer];
     const dot = /[.]/;
-    let decTest = dot.test(thing);
+    let decTest = dot.test(answer);
 
-    //if the length of the string is greater than 11 and there is a decimal, return a string that is properly rounded and is less than 11 digits long
-    if (arrayOfThing.length > 11 && decTest) {
-        arrayOfThing.splice(11, arrayOfThing.length).join("");
-        screen.textContent = arrayOfThing.join("");
-    } else if (arrayOfThing.length > 11) {
-        // exponentNotation(thing);
-        screen.textContent = Number(thing).toExponential(5);
+    //checks for an output greater than 11(the number of characters that can fit on the screen)
+    //if more than 11 and is a decimal, just cuts off the other numbers
+    //if more than 11 and is not a decimal, will give the answer in exponential notation
+    if (answerArray.length > 11 && decTest) {
+        answerArray.splice(11, answerArray.length).join("");
+        screen.textContent = answerArray.join("");
+    } else if (answerArray.length > 11) {
+        screen.textContent = Number(answer).toExponential(5);
+    } else if (answer === "NaN" || answer === "Infinity") {
+        screen.textContent = "lol idiot";
     } else {
-        screen.textContent = thing;
+        screen.textContent = answer;
+    }
+}
+
+//detects what button is pressed, what key is pressed without the need for a shit ton on document.addEventListeners and making consts
+function keyPush(evt) {
+    switch (evt.key || evt.originalTarget.id) {
+        case "one":
+        case "1":
+            addTextToScreen(1);
+            break;
+        case "two":
+        case "2":
+            addTextToScreen(2);
+            break;
+        case "three":
+        case "3":
+            addTextToScreen(3);
+            break;
+        case "four":
+        case "4":
+            addTextToScreen(4);
+            break;
+        case "five":
+        case "5":
+            addTextToScreen(5);
+            break;
+        case "six":
+        case "6":
+            addTextToScreen(6);
+            break;
+        case "seven":
+        case "7":
+            addTextToScreen(7);
+            break;
+        case "eight":
+        case "8":
+            addTextToScreen(8);
+            break;
+        case "nine":
+        case "9":
+            addTextToScreen(9);
+            break;
+        case "zero":
+        case "0":
+            addTextToScreen(0);
+            break;
+        case "add":
+        case "+":
+            getOperators(" + ");
+            break;
+        case "subtract":
+        case "-":
+            getOperators(" - ");
+            break;
+        case "multiply":
+        case "*":
+            getOperators(" * ");
+            break;
+        case "divide":
+        case "/":
+            getOperators(" / ");
+            break;
+        case "decimal":
+        case ".":
+            addDecimal();
+            break;
+        case "equals":
+        case "Enter":
+        case "=":
+            operate(screenContent.join(""));
+            break;
+        case "back":
+        case "Backspace":
+            backspace();
+            break;
+        case "clear":
+        case "Delete":
+        case "c":
+            clearScreen();
+            break;
     }
 }
